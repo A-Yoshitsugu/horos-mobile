454901d965f73377956ee3c0fd9b1b024f36438c
A-Yoshitsugu <a.yoshitsugu.gude@gmail.com>
2023-07-13 22:20:52 +0900

    change file

---
+++ lib/View/zodiac_details/aries.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class AriesPage extends StatelessWidget{
+  const AriesPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Zodiac Signs',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 4, 14, 70)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: '星座占い'),
+    );
+  }
+}
